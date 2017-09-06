f218c799605d19cbe528f1fcfee524eb442d1f75
LeslieL <leslie.liao@phimedia.tv>
2017-09-06 22:25:12 +0800

    adjust resume sth

--- js/showBar.js
+++ js/showBar.js
@@ -3,9 +3,9 @@ $(window).scroll(function() {
 		  	document.getElementById("bar1").style.width = "95%";
 		  	document.getElementById("bar2").style.width = "78%";
 		  	document.getElementById("bar3").style.width = "50%";
-		  	document.getElementById("bar4").style.width = "40%";  
+		  	document.getElementById("bar4").style.width = "50%";  
 		  	document.getElementById("bar5").style.width = "95%";
-		  	document.getElementById("bar6").style.width = "30%";
+		  	document.getElementById("bar6").style.width = "40%";
 		  	document.getElementById("bar7").style.width = "99%";
 		  	document.getElementById("bar8").style.width = "88%";
 		  	document.getElementById("bar9").style.width = "90%";
