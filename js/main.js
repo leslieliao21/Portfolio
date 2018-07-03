93aceef08ef7b24c2d6d1a9ed63495017977a440
Leslie Liao <leslieliao21@gmail.com>
2018-07-03 13:10:59 +0800

    adjust loader

--- js/main.js
+++ js/main.js
@@ -24,61 +24,64 @@
 
 
 
-$(function(){
-$(window).load(function() {
+$(function() {
+  $(window).load(function() {
     // Animate loader off screen
     $(".preloader").fadeOut("slow");
-});
-
-// var msg='';
-// var like_id='';
-
-function igotop(){
-	$('html,body').animate({scrollTop:0});
-}	
-
-$('.btnbacktop').scrollToggle();
-$('.btnbacktop').positionToggle({align:'bottom', position:280, marginX:30, marginY:30});
-$('.btnbacktop').click(function (){
-	igotop(); 
-});
-
-
-$('.hover').mouseleave({
-   function(){
-   	$(this).removeclass("hover");
-   }
- });
-
-
-
- $('.goresume').click(function(){
- 	$('#resume').show();
-  $('#mainall').hide();
- });
-
- $('.content').on('click','.goback', function(){
- 	igotop();
-	$('#resume').hide();
-  $('#mainall').show();
-});
-
-//上面兩段的2合1
-//如果你加載的元素中有圖片的話，那麼需要確保Masonry在所有圖片都加載完後才執行，需要調用如下代碼
-  var $container = $('#container');
-$container.imagesLoaded(function(){
-  $container.masonry({
-    itemSelector : '.item',
-    gutter: 20,
-    /*columnWidth : 320,*/
-    isFitWidth: true
   });
-});
+
+  function igotop() {
+    $('html,body').animate({
+      scrollTop: 0
+    });
+  }
+
+  $('.btnbacktop').scrollToggle();
+  $('.btnbacktop').positionToggle({
+    align: 'bottom',
+    position: 280,
+    marginX: 30,
+    marginY: 30
+  });
+  $('.btnbacktop').click(function() {
+    igotop();
+  });
+
+
+  $('.hover').mouseleave({
+    function() {
+      $(this).removeclass("hover");
+    }
+  });
+
+
+
+  $('.goresume').click(function() {
+    $('#resume').show();
+    $('#mainall').hide();
+  });
+
+  $('.content').on('click', '.goback', function() {
+    igotop();
+    $('#resume').hide();
+    $('#mainall').show();
+  });
+
+  //上面兩段的2合1
+  //如果你加載的元素中有圖片的話，那麼需要確保Masonry在所有圖片都加載完後才執行，需要調用如下代碼
+  var $container = $('#container');
+  $container.imagesLoaded(function() {
+    $container.masonry({
+      itemSelector: '.item',
+      gutter: 20,
+      /*columnWidth : 320,*/
+      isFitWidth: true
+    });
+  });
 
 
 });
 
 $(window).load(function() {
   $('#resume').load('./resume.html');
-
 });
