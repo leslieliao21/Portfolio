09387b807d0fb5d0831fd050ad6b84c970397fb6
Leslie Liao <leslieliao21@gmail.com>
2018-07-03 12:51:34 +0800

    adjust some text

--- js/showBar.js
+++ js/showBar.js
@@ -6,9 +6,9 @@ $(window).scroll(function() {
 		  	// document.getElementById("bar4").style.width = "50%";  
 		  	document.getElementById("bar5").style.width = "95%";
 		  	document.getElementById("bar6").style.width = "50%";
-		  	document.getElementById("bar7").style.width = "99%";
+		  	document.getElementById("bar7").style.width = "98%";
 		  	document.getElementById("bar8").style.width = "88%";
-		  	document.getElementById("bar9").style.width = "90%";
+		  	document.getElementById("bar9").style.width = "85%";
 		  	document.getElementById("bar10").style.width = "86%";
 		  	// document.getElementById("bar11").style.width = "78%";
 		  	document.getElementById("bar12").style.width = "70%";
